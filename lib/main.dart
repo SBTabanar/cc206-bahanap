1941c623e5758966c469a93206174299160e70ab
Sergei Benjamin Tabanar <sergeitabanar7@gmail.com>
2024-09-19 13:20:24 +0800

    Minor revisions

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'features/sign_up_page.dart'; // Import the SignUpPage
+import 'features/sign_up_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bahanap',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignUpPage(), // Set SignUpPage as the home widget
+      home: SignUpPage(),
     );
   }
 }
