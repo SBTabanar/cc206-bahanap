d6321edc30b1cd873aac9418732dc22cef96c086
eulerProof <joshuaanthony.sasi@wvsu.edu.ph>
2024-10-23 23:37:08 +0800

    Added Basic Interactability Functions

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
          primarySwatch: Colors.blue,
       ),
-      home: DashboardPage(),
+      home: const DashboardPage(),
     );
   }
 }
